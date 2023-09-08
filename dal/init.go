8ece13fa0a59a0726ca6864d46e0d0c73b04eef1
chenmingzhi <chenmingzhi@bytedance.com>
2023-09-08 16:39:28 +0800

    dev

--- dal/init.go
+++ dal/init.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var (
@@ -15,7 +16,8 @@ var (
 func InitDB() {
 	dsn := "root:@tcp(127.0.0.1:3306)/test?charset=utf8mb4&parseTime=True&loc=Local"
 	var err error
-	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+
 	if err != nil {
 		panic(err)
 	}
